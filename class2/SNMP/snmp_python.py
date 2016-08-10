2571e692570a5ca60259d32e07760735bf12c30c
Paul Kim <hongkim0@gmail.com>
2016-08-10 00:39:59 -0700

    modify snmp thin

--- class2/SNMP/snmp_python.py
+++ class2/SNMP/snmp_python.py
@@ -2,11 +2,15 @@
 from snmp_helper import snmp_get_oid, snmp_extract
 
 OID_list = ['1.3.6.1.2.1.1.1.0','1.3.6.1.2.1.1.5.0']
-IP = "184.105.247.70"
+OID_name = ['sysDscr', 'sysName']
+IP_list = ["184.105.247.70", "184.105.247.71"]
 COMMUNITY_STRING = "galileo"
 SNMP_PORT = 161
 
-a_device = (IP,COMMUNITY_STRING,SNMP_PORT)
-for oid in OID_list:
-    snmp_data = snmp_get_oid(a_device, oid)
-    print snmp_extract(snmp_data)
+for ip in IP_list:
+    print "<<<<<<<<<",ip,">>>>>>>>>"
+    a_device = (ip,COMMUNITY_STRING,SNMP_PORT)
+    for i in range(len(OID_list)):
+       print "==",OID_name[i],"=="
+       snmp_data = snmp_get_oid(a_device, OID_list[i])
+       print snmp_extract(snmp_data)
